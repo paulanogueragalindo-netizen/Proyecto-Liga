0650038daf235bf266883014132c5f63736a842d
Paula Noguera <paulanogueragalindo@gmail.com>
2026-04-06 21:06:59 -0500

    Ver partidos y poner todo en funciones

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ std::string separar_texto(std::string texto, char cc){
     std::stringstream separador(texto);
     getline(separador, palabra_indivual, cc);
     getline(separador, palabra_indivual, cc);
+
+    // getline(separador, dato, ';')
     return palabra_indivual;
 }
 
@@ -58,6 +60,77 @@ int cargar_configuracion(std::string& titulo, int& puntos_victoria, int& puntos_
     }
 }
 
+void registrar_equipo(std::vector<std::string>& equipos){
+    std::cin.ignore(1000, '\n');
+    std::cout << "Estos son los equipos disponibles:" << std::endl;
+    for(int pos = 0; pos < equipos.size(); pos++){
+        std::cout << " - " << equipos[pos] << std::endl;
+    }
+
+    std::string EquipoLocal;
+    std::cout<< "Introduzca el equipo local:" << std::endl;
+    std::getline(std::cin, EquipoLocal);
+    
+    std::string EquipoVisitante;
+    std::cout << "Introduzca el equipo visitante:" << std::endl;
+    std::getline(std::cin, EquipoVisitante);
+    
+    int puntaje_local, puntaje_visitante;
+    std::cout << "Introduzca el puntaje del equipo local" << std::endl;
+    std::cin >> puntaje_local;
+    std::cout << "Introduzca el puntaje el equipo visitante" << std::endl;
+    std::cin >> puntaje_visitante;
+    
+    bool encontrado_local = false;
+    for(int i=0; i<equipos.size(); i++){
+        if(equipos[i] == EquipoLocal){
+            encontrado_local = true;
+        }
+    }
+
+    if(encontrado_local != true){
+        std::cout<< "No se encontro el equipo local ingresado" <<std::endl;
+        return;
+    }
+
+    bool encontrado_visitante = false;
+    for(int i=0; i<equipos.size(); i++){
+        if(equipos[i] == EquipoVisitante){
+            encontrado_visitante = true;
+        }
+    }
+
+    if(encontrado_visitante != true){
+        std::cout<< "No se encontro el equipo visitante ingresado" << std::endl;
+        return;
+    }
+
+    
+    if(!(puntaje_local >= 0 && puntaje_visitante >= 0)){
+        std:: cout<< "No es valido"<< std::endl;
+        return;
+    }
+
+    // Registro en el archivo partidos.txt
+    std::ofstream File_2("../data/partidos.txt", std::ios::app); 
+    File_2 << EquipoLocal << ";" << EquipoVisitante << ";" << puntaje_local << ";" << puntaje_visitante << "\n";
+}
+
+void ver_partidos(){
+    std::ifstream partidos ("../data/partidos.txt");
+    std::string linea;
+    while(getline(partidos, linea)){
+        std::string dato;
+        std::stringstream separador (linea);
+        while(getline(separador,dato, ';')){
+            std::cout <<dato << " | ";
+        }
+        std::cout << "\n";
+    }
+    partidos.close();
+}
+
+
 int main(){
     std::string titulo; 
     int puntos_victoria, puntos_empate, puntos_derrota;
@@ -92,46 +165,21 @@ int main(){
         std::cout << "Escoger un numero:"<< std::endl;
         std::cin >> numero;
 
-        // switch tomar una variable/expresion y hacer casos dependiendo de su valor
-
         switch (numero){
             case 1:
                 //
                 break;
             case 2:{
-                std::cin.ignore(1000, '\n');
-                std::cout << "Estos son los equipos disponibles:" << std::endl;
-                for(int pos = 0; pos < equipos.size(); pos++){
-                    std::cout << " - " << equipos[pos] << std::endl;
-                }
-                // Equipo local
-                std::string EquipoLocal;
-                std::cout<< "Introduzca el equipo local:" << std::endl;
-                std::getline(std::cin, EquipoLocal);
-                
-                // Equipo visitante
-                std::string EquipoVisitante;
-                std::cout << "Introduzca el equipo visitante:" << std::endl;
-                std::getline(std::cin, EquipoVisitante);
-                
-                // Marcador
-                int puntaje_local, puntaje_visitante;
-                std::cout << "Introduzca el puntaje del equipo local" << std::endl;
-                std::cin >> puntaje_local;
-                std::cout << "Introduzca el puntaje el equipo visitante" << std::endl;
-                std::cin >> puntaje_visitante;
-                
-                // Registro en el archivo partidos.txt
-                std::ofstream File_2("../data/partidos.txt", std::ios::app); 
-                File_2 << EquipoLocal << ";" << EquipoVisitante << ";" << puntaje_local << ";" << puntaje_visitante << "\n";
+                registrar_equipo(equipos);
                 break;
             }
             case 3:
 
                 break;
-            case 4:
-
+            case 4:{
+                ver_partidos();
                 break;
+            }
             case 5:
                 menu = false;
                 break;
